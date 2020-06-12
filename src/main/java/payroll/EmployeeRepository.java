5156eb4aafad5d205688349020cd29a39cca7730
cheol-hee-Lee <201802141@o.cnu.ac.kr>
2020-06-13 01:32:29 +0900

    1. Add DatabaseLoader.java 2. Update EmployeeRepository.java

--- src/main/java/payroll/EmployeeRepository.java
+++ src/main/java/payroll/EmployeeRepository.java
@@ -1,7 +1,8 @@
 package payroll;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 
-interface EmployeeRepository extends JpaRepository<Employee, Long> {
+interface EmployeeRepository extends CrudRepository<Employee, Long> {
 
 }
